4b30cf4898bf3572d17a0243229a16e85271b3fe
manishparihar948 <manish.parihar948@gmail.com>
2023-07-10 23:14:58 +0200

    Initial commit

---
+++ MobileCV/MobileCV/MobileCVApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileCVApp.swift
+//  MobileCV
+//
+//  Created by Manish Parihar on 10.07.23.
+//
+
+import SwiftUI
+
+@main
+struct MobileCVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
