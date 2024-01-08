2667b065d0f126303c6ced45a1ef654d1884bb86
manishparihar948 <manish.parihar948@gmail.com>
2024-01-08 23:49:26 +0100

    Onboarding feature added

---
+++ MobileCV/MobileCV/Base/OnBoarding/View/OnBoardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnBoardingView.swift
+//  MobileCV
+//
+//  Created by Manish Parihar on 08.01.24.
+//
+
+import SwiftUI
+
+struct OnBoardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnBoardingView()
+}
